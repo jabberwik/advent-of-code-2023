5543dfeeca5de7fe000481f4392a31e1984c9913
Daniel Laughland <daniel@laughland.com>
2023-12-02 22:01:02 -0600

    Day 1 Part 2

--- day-01/Program.cs
+++ day-01/Program.cs
@@ -1,6 +1,11 @@
-﻿using var httpClient = new HttpClient();
+﻿using System.Text.RegularExpressions;
+
+using var httpClient = new HttpClient();
 httpClient.DefaultRequestHeaders.Add("Cookie", Environment.GetEnvironmentVariable("AOC_SESSION"));
 var inputStream = await httpClient.GetStreamAsync("https://adventofcode.com/2023/day/1/input");
+
+var digits = new[] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+var numberMatch = new Regex(@$"(?=(\d|{string.Join('|', digits)}))");
 
 using var reader = new StreamReader(inputStream);
 
@@ -10,10 +15,15 @@ while (!reader.EndOfStream)
 {
   var line = await reader.ReadLineAsync();
 
-  var first = line!.First(char.IsDigit);
-  var last = line!.Last(char.IsDigit);
+  var matches = numberMatch.Matches(line!);
+
+  var firstStr = matches.First().Groups[1].Value;
+  var lastStr = matches.Last().Groups[1].Value;
+
+  var first = int.TryParse(firstStr, out var n1) ? n1 : Array.IndexOf(digits, firstStr) + 1;
+  var last = int.TryParse(lastStr, out var n2) ? n2 : Array.IndexOf(digits, lastStr) + 1;
   
-  total += int.Parse($"{first}{last}");
+  total += (first * 10) + last;
 }
 
 Console.WriteLine(total);
