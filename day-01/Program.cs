4b185f0243e60f362e167fe9594b36cc135b1266
Daniel Laughland <daniel@laughland.com>
2023-12-02 22:00:48 -0600

    Day 1 Part 1

---
+++ day-01/Program.cs
@@ -0,0 +1,19 @@
+﻿using var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Add("Cookie", Environment.GetEnvironmentVariable("AOC_SESSION"));
+var inputStream = await httpClient.GetStreamAsync("https://adventofcode.com/2023/day/1/input");
+
+using var reader = new StreamReader(inputStream);
+
+int total = 0;
+
+while (!reader.EndOfStream)
+{
+  var line = await reader.ReadLineAsync();
+
+  var first = line!.First(char.IsDigit);
+  var last = line!.Last(char.IsDigit);
+  
+  total += int.Parse($"{first}{last}");
+}
+
+Console.WriteLine(total);
