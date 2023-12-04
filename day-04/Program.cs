5aa168f4966dc286efda764561ce3902b15a386d
Daniel Laughland <daniel@laughland.com>
2023-12-03 23:34:25 -0600

    Day 4 Part 2

--- day-04/Program.cs
+++ day-04/Program.cs
@@ -1,23 +1,26 @@
 ﻿using var httpClient = new HttpClient();
 httpClient.DefaultRequestHeaders.Add("Cookie", Environment.GetEnvironmentVariable("AOC_SESSION"));
-var inputStream = await httpClient.GetStreamAsync("https://adventofcode.com/2023/day/4/input");
+var input = await httpClient.GetStringAsync("https://adventofcode.com/2023/day/4/input");
+var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
-using var reader = new StreamReader(inputStream);
+var copies = Enumerable.Repeat(1, lines.Length).ToArray();
 
-int total = 0;
-
-while (!reader.EndOfStream)
+for (var cardIndex = 0; cardIndex < lines.Length; cardIndex++)
 {
-  var line = await reader.ReadLineAsync();
-
-  var numbers = line.Split(':')[1].Split('|');
+  var numbers = lines[cardIndex].Split(':')[1].Split('|');
   
   var winningNumbers = numbers[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
   var haveNumbers = numbers[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
 
   var wins = haveNumbers.Count(n => winningNumbers.Contains(n));
 
-  if (wins > 0) total += (int)Math.Pow(2, wins - 1);
+  Console.WriteLine($"Card {cardIndex} has {wins} wins on {copies[cardIndex]} copies");
+
+  for (var winIndex = cardIndex + 1; winIndex <= cardIndex + wins && winIndex < copies.Length; winIndex++)
+  {
+    copies[winIndex] += copies[cardIndex];
+    Console.WriteLine($"Card {winIndex} now has {copies[winIndex]} copies");
+  }
 }
 
-Console.WriteLine(total);
+Console.WriteLine(copies.Sum());
